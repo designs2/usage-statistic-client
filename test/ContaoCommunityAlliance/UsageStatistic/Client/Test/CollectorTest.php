f2469b33f6a3345030522c3855f1a0ed0929342e
Tristan Lins <tristan.lins@bit3.de>
2014-08-07 14:59:50 +0200

    Add header to each php file.

--- test/ContaoCommunityAlliance/UsageStatistic/Client/Test/CollectorTest.php
+++ test/ContaoCommunityAlliance/UsageStatistic/Client/Test/CollectorTest.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * Usage statistic client for Contao Open Source CMS
+ *
+ * PHP version 5
+ *
+ * @copyright  2014 Contao Community Alliance <http://c-c-a.org>
+ * @author     Tristan Lins <t.lins@c-c-a.org>
+ * @package    contao-community-alliance/usage-statistic-client
+ * @license    LGPL-3.0+ <http://opensource.org/licenses/LGPL-3.0>
+ * @filesource
+ */
+
 namespace ContaoCommunityAlliance\UsageStatistic\Client\Test;
 
 use ContaoCommunityAlliance\Contao\Events\Cron\CronEvents;
